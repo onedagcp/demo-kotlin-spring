e2d9a3b2a05933f410f1ef4d6a00aeff4e458f82
oonedas <onedas@zoff.com>
2019-06-26 18:06:01 +0900

    add thymeleaf

--- src/main/kotlin/com/example/demo/controller/GreetingController.kt
+++ src/main/kotlin/com/example/demo/controller/GreetingController.kt
@@ -1,14 +1,19 @@
 package com.example.demo.controller
 
+import com.example.demo.service.MemoService
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("greeting")
-class GreetingController {
+class GreetingController(private val service: MemoService) {
+
+    private val log = LoggerFactory.getLogger(MemoController::class.java)
 
     @GetMapping("/hello")
     fun hello(
@@ -17,4 +22,20 @@ class GreetingController {
         model.addAttribute("name", name)
         return "greeting"
     }
+
+    @GetMapping("/hello2")
+    fun hello2(
+            @RequestParam(value = "id", required = false, defaultValue = "1") name: Long,
+            model: Model): String {
+
+        val id : Long = name.toLong()
+        val memo = service.findById(id)
+
+        log.debug("store memo : {}", memo)
+
+        model.addAttribute("name", name)
+        model.addAttribute("memo", memo)
+        return "greeting"
+    }
+
 }
